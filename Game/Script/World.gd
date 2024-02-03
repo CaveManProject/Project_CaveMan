8ab07c7ae4c8fcbf648d48c1f900c0af6033059b
Branislav Hozza <branislav.hozza@soitron.com>
2024-02-03 22:19:16 +0100

    :recycle: better chunk generation

--- Game/Script/World.gd
+++ Game/Script/World.gd
@@ -24,27 +24,25 @@ func cordsToType(cords: Vector2) -> TileType:
 
 var rng = RandomNumberGenerator.new()
 
+@export var MAX_CHUNK_SIZE: int = 8
+
 class ChunkGenerator:
 	var pos: Vector2
 	var dir: Vector2
+	var chunk_size: int
 
 var CellSize = Vector2(16, 16)
 
-@export var WIDTH: int = 256
-@export var HEIGHT: int = 256
+@export var MAP_WIDTH: int = 20
+@export var MAP_HEIGHT: int = 20
 
-var MAP_WIDTH = WIDTH/CellSize.x
-var MAP_HEIGHT = HEIGHT/CellSize.y
-@export var SAFE_ZONE_RADIUS = 5
+@export var SAFE_ZONE_RADIUS: int = 4
 
-var ORE_CHUNK_CHANCE = 0.8
-var GENERATOR_MOVE_CHANCE = 0.5
-var GENERATOR_DESTROY_CHANCE = 0.8
-var GENERATOR_SPAWN_CHANCE = 0.8
+@export var GENERATOR_DESTROY_CHANCE: float = 0.1
+@export var GENERATOR_SPAWN_CHANCE: float = 1
 
-var MAX_ITERATION = 10
-var MAX_GENERATORS = 5
-var MAX_FIL_PERCENT = 0.3
+@export var MAX_ITERATION: int = 10
+@export var MAX_GENERATORS: int = 10
 
 var DIRECTIONS: Array[Vector2] = [Vector2(-1, 0), Vector2(1, 0), Vector2(0, 1), Vector2(0, -1)]
 
@@ -52,16 +50,22 @@ var grid = []
 
 var generators: Array[ChunkGenerator] = []
 
-
-func getRandomDirection() -> Vector2:
-	return DIRECTIONS[rng.randi()%4]
-	
 func isInSafeZone(x: int, y: int) -> bool:
 	var t_x = x - MAP_WIDTH/2
 	var t_y = y - MAP_HEIGHT/2
 	var radius = sqrt(pow(t_x, 2) + pow(t_y,2))
 	return radius < SAFE_ZONE_RADIUS
-	
+
+func getRandomDirection() -> Vector2:
+	return DIRECTIONS[rng.randi()%4]
+
+func getSafeRandomPosition() -> Vector2:
+	var ran_x = rng.randi_range(1, MAP_WIDTH-1)
+	var ran_y = rng.randi_range(1, MAP_HEIGHT-1)
+	while isInSafeZone(ran_x, ran_y):
+		ran_x = rng.randi_range(1, MAP_WIDTH-1)
+		ran_y = rng.randi_range(1, MAP_HEIGHT-1)
+	return Vector2(ran_x, ran_y)
 
 func initGrid():
 	grid = []
@@ -76,37 +80,36 @@ func initGrid():
 func initGenerators():
 	var generator: ChunkGenerator = ChunkGenerator.new()
 	generator.dir = getRandomDirection()
-	generator.pos = Vector2(MAP_WIDTH/2, MAP_HEIGHT/2)
+	generator.pos = getSafeRandomPosition()
+	generator.chunk_size = 0
 	generators = [generator]
 
 func createChunks():
 	initGenerators()
 	var iteration: int = 0
-	var tile_count: int = 0
 	while iteration < MAX_ITERATION:
 		# Change direction, with chance
-		for i in range(generators.size()):
-			if rng.randf() < GENERATOR_MOVE_CHANCE:
-				generators[i].dir = getRandomDirection()
+		for generator in generators:
+			generator.dir = getRandomDirection()
+			
+		# Random: Maybe destroy generator?
+		for generator in generators:
+			if rng.randf() < GENERATOR_DESTROY_CHANCE:
+				generators.erase(generator);
+				break; # Destroy only one generator per iteration
 		
-		# Random: Maybe destroy walker?
-		for i in range(generators.size()):
-			if rng.randf() < GENERATOR_DESTROY_CHANCE and generators.size() > 1:
-				generators.remove_at(i);
-				break; # Destroy only one walker per iteration
-		
-		# Spawn new walkers, with chance
-		for i in range(generators.size()):
+		# Spawn new generator, with chance
+		for _g in generators:
 			if rng.randf() < GENERATOR_SPAWN_CHANCE and generators.size() < MAX_GENERATORS:
 				var generator = ChunkGenerator.new()
 				generator.dir = getRandomDirection()
-				generator.pos = generators[i].pos
+				generator.pos = getSafeRandomPosition()
+				print("Spawning at: ", generator.pos)
+				generator.chunk_size = 0
 				generators.append(generator)
 	
-		# Advance walkers
-		for i in range(generators.size()):
-			var generator = generators[i]
-			
+		# Advance generator
+		for generator in generators:
 			if (generator.pos.x + generator.dir.x >= 1 and 
 				generator.pos.x + generator.dir.x < MAP_WIDTH-1 and
 				generator.pos.y + generator.dir.y >= 1 and
@@ -115,13 +118,12 @@ func createChunks():
 					generator.pos += generator.dir
 					if grid[generator.pos.x][generator.pos.y] == TileType.STONE:
 						grid[generator.pos.x][generator.pos.y] = TileType.COAL
-						tile_count += 1
+						generator.chunk_size += 1
 						
-						if float(tile_count)/float(MAP_WIDTH*MAP_HEIGHT) >= MAX_FIL_PERCENT:
-							return
+						if generator.chunk_size == MAX_CHUNK_SIZE:
+							generators.erase(generator)
 		
 		iteration += 1
-		print("Tile count: ",tile_count)
 
 
 func printChunks():
@@ -139,7 +141,6 @@ func spawnTiles():
 				TileType.STONE:
 					tileMap.set_cell(0, Vector2(t_x, t_y), 0, typeToCords(TileType.STONE))
 				TileType.COAL:
-					print("Setting coal", x, " ", y)
 					tileMap.set_cell(0, Vector2(t_x, t_y), 0, typeToCords(TileType.COAL))
 					
 
@@ -148,7 +149,6 @@ func _ready():
 	initGrid()
 	initGenerators()
 	createChunks()
-	# printChunks()
 	spawnTiles()
 
 func blockInRadius(playerCords: Vector2) -> bool:
@@ -162,7 +162,6 @@ func mineBlock(playerCords: Vector2):
 	for dir in DIRECTIONS:
 		var newDir = playerCords + dir
 		if grid[newDir.x][newDir.y] != TileType.NIL:
-			print("Removing cell")
 			tileMap.erase_cell(0, newDir)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -170,4 +169,12 @@ func _process(delta):
 	var playerCoords = tileMap.local_to_map(player.global_position)
 	if Input.is_action_just_pressed("e") and blockInRadius(playerCoords) :
 		mineBlock(playerCoords)
+	
+	if Input.is_action_just_pressed("space"):
+		print("Generating")
+		initGrid()
+		initGenerators()
+		createChunks()
+		spawnTiles()
+		
 		
