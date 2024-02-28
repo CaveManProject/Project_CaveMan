cab35fdc9a49561346a220fb2cf4bf6390f6bb83
Branislav Hozza <brankohozza@gmail.com>
2024-02-28 15:58:09 +0100

    :bug: fix binding issues

--- Script/MainMenu/keybind/RebindMenu.cs
+++ Script/MainMenu/keybind/RebindMenu.cs
@@ -4,128 +4,131 @@ using Godot;
 
 namespace Caveman.Keybind
 {
-    public partial class RebindMenu : Control
-    {
-        private Button button;
-        public string actionName = "up";
+	public partial class RebindMenu : Control
+	{
+		private Button _button;
+		private Label _label;
 
-        public override void _Ready()
-        {
-            SetProcessUnhandledKeyInput(false);
-            SetActionName();
-            SetTextForKey();
-        }
+		[Export]
+		public string actionName;
 
-        private void SetActionName()
-        {
-            string label = "unassigned";
-            label = actionName switch
-            {
-                "up" => "UP",
-                "down" => "DOWN",
-                "left" => "LEFT",
-                "right" => "RIGHT",
-                "mb_left" => "MB LEFT",
-                "mb_right" => "MB RIGHT",
-                "i" => "I",
-                "e" => "E",
-                "space" => "SPACEBAR",
-                "pause" => "ESC",
-                _ => ""
-            };
-        }
+		public override void _Ready()
+		{
+			_button = GetNode<Button>("HBoxContainer/Button");
+			_label = GetNode<Label>("HBoxContainer/action_button");
+			SetProcessUnhandledKeyInput(false);
+			SetActionName();
+			SetTextForKey();
+		}
 
-        private void SetTextForKey()
-        {
-            var actionEvents = InputMap.ActionGetEvents(actionName);
-            if (!actionEvents.Any())
-            {
-                return;
-            }
-            var actionEvent = actionEvents[0];
-            if (actionEvent is InputEventKey)
-            {
-                var actionKeycode = OS.GetKeycodeString(((InputEventKey)actionEvent).PhysicalKeycode);
-                button.Text = actionKeycode;
-            }
-            else if (actionEvent is InputEventMouseButton)
-            {
-                var actionKeycode = OS.GetKeycodeString((Key)((InputEventMouseButton)actionEvent).ButtonIndex);
-                button.Text = "Mouse Button " + actionKeycode;
-            }
-        }
+		private void SetActionName()
+		{
+			_label.Text = actionName switch
+			{
+				"up" => "UP",
+				"down" => "DOWN",
+				"left" => "LEFT",
+				"right" => "RIGHT",
+				"mb_left" => "MB LEFT",
+				"mb_right" => "MB RIGHT",
+				"i" => "I",
+				"e" => "E",
+				"space" => "SPACEBAR",
+				"pause" => "ESC",
+				_ => "unassigned"
+			};
+		}
 
-        private void OnButtonToggled(bool buttonPressed)
-        {
-            if (buttonPressed)
-            {
-                button.Text = "Press any key.....";
-                SetProcessUnhandledKeyInput(buttonPressed);
-                foreach (var node in GetTree().GetNodesInGroup("hotkey_button"))
-                {
-                    if (node is RebindMenu)
-                    {
-                        var menu = node as RebindMenu;
-                        if (menu.actionName != this.actionName)
-                        {
-                            menu.button.ToggleMode = false;
-                            menu.SetProcessUnhandledKeyInput(false);
-                        }
-                    }
-                }
-            }
-            else
-            {
-                foreach (var node in GetTree().GetNodesInGroup("hotkey_button"))
-                {
-                    if (node is RebindMenu)
-                    {
-                        var menu = node as RebindMenu;
-                        if (menu.actionName != this.actionName)
-                        {
-                            menu.button.ToggleMode = true;
-                            menu.SetProcessUnhandledKeyInput(false);
-                        }
-                    }
-                }
-                SetTextForKey();
-            }
-        }
+		private void SetTextForKey()
+		{
+			var actionEvents = InputMap.ActionGetEvents(actionName);
+			if (!actionEvents.Any())
+			{
+				return;
+			}
+			if (actionEvents[0] is InputEventKey keyEvent)
+			{
+				GD.Print(keyEvent.PhysicalKeycode);
+				var actionKeycode = OS.GetKeycodeString(keyEvent.PhysicalKeycode);
+				_button.Text = actionKeycode;
+			}
+			else if (actionEvents[0] is InputEventMouseButton button)
+			{
+				var actionKeycode = OS.GetKeycodeString((Key)button.ButtonIndex);
+				_button.Text = "Mouse Button " + actionKeycode;
+			}
+		}
 
-        private void UnhandledKeyInput(InputEventKey eventKey)
-        {
-            RebindActionKey(eventKey);
-            button.ButtonPressed = false;
-        }
+		private void OnButtonToggled(bool buttonPressed)
+		{
+			GD.Print("Button Pressed: " + buttonPressed);
+			if (buttonPressed)
+			{
+				_button.Text = "Press any key.....";
+				SetProcessUnhandledKeyInput(buttonPressed);
+				// Disable other buttons
+				foreach (var node in GetTree().GetNodesInGroup("hotkey_button"))
+				{
+					if (node is RebindMenu menu)
+					{
+						if (menu.actionName != this.actionName)
+						{
+							menu._button.ToggleMode = false;
+							menu.SetProcessUnhandledKeyInput(false);
+						}
+					}
+				}
+			}
+			else
+			{
+				// Enable other buttons
+				foreach (var node in GetTree().GetNodesInGroup("hotkey_button"))
+				{
+					if (node is RebindMenu menu)
+					{
+						if (menu.actionName != this.actionName)
+						{
+							menu._button.ToggleMode = true;
+							menu.SetProcessUnhandledKeyInput(false);
+						}
+					}
+				}
+				SetTextForKey();
+			}
+		}
 
-        private void RebindActionKey(InputEventKey eventKey)
-        {
-            var isDuplicate = false;
-            var actionEvent = eventKey;
-            var actionKeycode = OS.GetKeycodeString(((InputEventKey)actionEvent).PhysicalKeycode);
-            foreach (var node in GetTree().GetNodesInGroup("hotkey_button"))
-            {
-                if (node is RebindMenu)
-                {
-                    var menu = node as RebindMenu;
-                    if (menu.actionName != this.actionName)
-                    {
-                        if (menu.button.Text == actionKeycode)
-                        {
-                            isDuplicate = true;
-                            break;
-                        }
-                    }
-                }
-            }
-            if (!isDuplicate)
-            {
-                InputMap.ActionEraseEvents(actionName);
-                InputMap.ActionAddEvent(actionName, eventKey);
-                SetProcessUnhandledKeyInput(false);
-                SetTextForKey();
-                SetActionName();
-            }
-        }
-    }
+		public override void _UnhandledKeyInput(InputEvent @event)
+		{
+			if (@event is InputEventKey keyEvent)
+			{
+				GD.Print("Unhandled Key Input: " + keyEvent.Keycode);
+				RebindActionKey(keyEvent);
+				_button.ButtonPressed = false;
+			}
+		}
+
+		private void RebindActionKey(InputEventKey keyEvent)
+		{
+			var actionKeycode = OS.GetKeycodeString(keyEvent.PhysicalKeycode);
+			// Check if the key is already assigned to another action
+			foreach (var node in GetTree().GetNodesInGroup("hotkey_button"))
+			{
+				if (node is RebindMenu menu)
+				{
+					if (menu.actionName != this.actionName && menu._button.Text == actionKeycode)
+					{
+						GD.Print("Key already assigned to another action");
+						return;
+					}
+				}
+			}
+			// If the key is not assigned to another action, rebind the action
+			InputMap.ActionEraseEvents(actionName);
+			InputMap.ActionAddEvent(actionName, keyEvent);
+			SetProcessUnhandledKeyInput(false);
+			SetTextForKey();
+			SetActionName();
+
+		}
+	}
 }
